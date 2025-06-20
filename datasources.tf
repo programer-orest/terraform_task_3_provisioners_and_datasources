6f9b9f074c507d719d7c5338be51b458b96d7043
Orest Yakymiv <akimivorest@gmail.com>
2025-06-20 15:52:46 +0300

    test

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,24 @@
 data "azurerm_resource_group" "example" {
   name = "ex3mate"
 }
+
+data "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_network_interface" "example" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_subnet" "example" {
+  name                 = "${var.prefix}-internal"
+  virtual_network_name = data.azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_virtual_machine" "example" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
